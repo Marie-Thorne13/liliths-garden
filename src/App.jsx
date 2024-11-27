f594d591a010d47cda2db4ac97e55eacc5941097
Toni Anguiano <toni.anguiano@sjsu.edu>
2024-11-26 16:42:31 -0800

    update public folder

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <main>
-      <h1 className='text-5xl text-orange-500 fond-bold'>Lilith's Garden</h1>
+      <h1 className='text-5xl text-teal-500 font-bold'>Lilith's Garden</h1>
       </main>
   )
 }
