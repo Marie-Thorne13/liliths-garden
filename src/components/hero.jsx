9a9b519a667a912468d0a9c319ef5b15d6a5f77a
Toni Anguiano <toni.anguiano@sjsu.edu>
2024-11-27 23:28:39 -0800

    Update hero.jsx

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,6 +1,14 @@
 const hero = () => {
   return (
-    <div>hero</div>
+    <><div className='relative h-dvh w-screen overflow-x-hidden'>hero</div>
+      <div id="video-frame" className="relative z-10 h-dvh w-screen overflow-x-hidden 
+    rounded-lg bg-blue-75" >
+        <div>
+            <div className="msk-clip-path">
+            </div>
+         </div>
+    </div>
+      </>
   )
 }
 
