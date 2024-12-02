3c93b39fb4621e33a262634024a9d2d2a04281c0
Toni Anguiano <toni.anguiano@sjsu.edu>
2024-12-02 09:50:15 -0800

    update

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,12 +1,21 @@
+const handleMiniVdClick = () => {
+  
+}
+
 const Hero = () => {
   return (
-    <><div className='relative h-dvh w-screen overflow-x-hidden'>Hero</div>
+    <><div className='relative h-dvh w-screen overflow-x-hidden'>
       <div id="video-frame" className="relative z-10 h-dvh w-screen overflow-x-hidden 
     rounded-lg bg-blue-75" >
         <div>
-            <div className="msk-clip-path">
+            <div className="msk-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden
+            rounded-lg ">
+              <div>
+                MiniVideoPlayer
+              </div>
             </div>
          </div>
+      </div>
     </div>
       </>
   )
