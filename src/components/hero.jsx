486ff4bdd4698ac390a86d4e8acc695e85f578ee
Toni Anguiano <toni.anguiano@sjsu.edu>
2024-12-02 11:42:35 -0800

    update

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -2,18 +2,19 @@ import { useRef, useState } from "react";
 
 const Hero = () => {
 
-  const [currentIndex, setcurrentIndex] = useState(1);
-  const [hasClicked, sethasClicked] = useState(false);
-  const [loading, setLoading] = useState(true);
-  const [loadedVideos, setloadedVideos] = useState(0);
-
-  const totalVideos = 4;
-  const nextVideoRef = useRef(null);
+    const [currentIndex, setCurrentIndex] = useState(1);
+    const [hasClicked, setHasClicked] = useState(false);
+  
+    const [loading, setLoading] = useState(true);
+    const [loadedVideos, setLoadedVideos] = useState(0);
+  
+    const totalVideos = 4;
+    const nextVideoRef = useRef(null);
 
 const handleMiniVdClick = () => {
-  sethasClicked(true);
+  setHasClicked(true);
 
-  setcurrentIndex((prevIndex) => prevIndex + 1);
+  setCurrentIndex((prevIndex) => prevIndex + 1);
 };
 
 const getVideoSrc = (index) => 'videos/hero-$(index).mp4'
@@ -23,7 +24,7 @@ const getVideoSrc = (index) => 'videos/hero-$(index).mp4'
       <div id="video-frame" className="relative z-10 h-dvh w-screen overflow-x-hidden 
     rounded-lg bg-blue-75" >
         <div>
-            <div className="msk-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden
+            <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden
             rounded-lg ">
               <div onClick={handleMiniVdClick} className='origin-center'>
               <video
