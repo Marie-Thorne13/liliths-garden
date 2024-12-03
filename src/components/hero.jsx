31bb8c56aa65108497c4eddd98fa9faf1a3f8037
Toni Anguiano <toni.anguiano@sjsu.edu>
2024-12-02 17:30:30 -0800

    Update hero.jsx

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -69,7 +69,11 @@ const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
                 G<b>a</b>ming
                 </h1>
             <div className='absolute left-0 top-0 z-40 size-full'>
-
+              <div className='mt-24 px-5 sm:px-10'>
+                 <h1 className='special-font hero-heading text-blue-100'>
+                    redefi<b>n</b>e
+                    </h1>
+              </div>
             </div>
       </div>
     </div>
