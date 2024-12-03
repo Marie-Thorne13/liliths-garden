5a99bf3dcc32711b716c80e3ad8bb526c22c5cc4
Toni Anguiano <toni.anguiano@sjsu.edu>
2024-12-02 17:25:02 -0800

    Update hero.jsx

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -48,8 +48,29 @@ const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
               <video 
                 ref={nextVideoRef}
                 src={getVideoSrc(currentIndex)}
+                loop
+                muted
+                id='next-video'
+                className='absolute-center invisible absolute z-10 size-64 object-cover object-center'
+                onLoadedData={handleVideoLoad}
+              />
+              <video
+                src={getVideoSrc(currentIndex === totalVideos - 1
+                    ? 1 : currentIndex)}
+                autoPlay
+                loop
+                muted
+                className='absolute left-0 top-0 size-full object-cover object-center'
+                onLoadedData={handleVideoLoad}
               />
          </div>
+             <h1 className='special-font hero-heading absolute bottom-5
+                right-5 z-40 text-blue-75'>
+                G<b>a</b>ming
+                </h1>
+            <div className='absolute left-0 top-0 z-40 size-full'>
+
+            </div>
       </div>
     </div>
       </>
