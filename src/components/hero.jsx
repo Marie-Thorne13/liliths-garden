80cb6529d9206f13515e0a233969baacfce1d8fb
Toni Anguiano <toni.anguiano@sjsu.edu>
2024-12-02 16:48:11 -0800

    Update hero.jsx

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -8,13 +8,19 @@ const Hero = () => {
     const [loading, setLoading] = useState(true);
     const [loadedVideos, setLoadedVideos] = useState(0);
   
-    const totalVideos = 4;
+    const totalVideos = 3;
     const nextVideoRef = useRef(null);
+
+    const handleVideoLoad = () => {
+        setLoadedVideos((prev) => prev + 1);
+    }
+
+    const upcomingVideoIndex = (currentIndex % totalVideos) + 1;
 
 const handleMiniVdClick = () => {
   setHasClicked(true);
 
-  setCurrentIndex((prevIndex) => (prevIndex) + 1);
+  setCurrentIndex(upcomingVideoIndex);
 };
 
 const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
@@ -26,7 +32,8 @@ const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
         <div>
             <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden
             rounded-lg ">
-              <div onClick={handleMiniVdClick} className='origin-center'>
+              <div onClick={handleMiniVdClick} className='origin-center scale-50 opacity-0 
+              transition-all duration-500 ease-in hover:scale-100 hover:opacity-100'>
               <video
                   ref={nextVideoRef}
                   src={getVideoSrc(currentIndex + 1)}
@@ -34,6 +41,7 @@ const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
                   muted
                   id="current-video"
                   className="size-64 origin-center scale-150 object-cover object-center"
+                  onLoadedData={handleVideoLoad}
                 />
               </div>
             </div>
