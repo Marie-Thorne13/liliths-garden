5a36cbd9df3ca9a1c7b7835b8b9af096dbd2b923
Toni Anguiano <toni.anguiano@sjsu.edu>
2024-12-17 00:21:48 -0800

    Update hero.jsx

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -68,15 +68,15 @@ const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
          </div>
              <h1 className='special-font hero-heading absolute bottom-5
                 right-5 z-40 text-blue-75'>
-                G<b>a</b>ming
+                thor<b>n</b>e
                 </h1>
             <div className='absolute left-0 top-0 z-40 size-full'>
               <div className='mt-24 px-5 sm:px-10'>
                  <h1 className='special-font hero-heading text-blue-100'>
-                    redefi<b>n</b>e
+                    m<b>a</b>rie
                     </h1>
                     <p className='mb-5 max-w-64 font-robert-regular text-blue-100'>
-                        Enter The Metagame Layer
+                        Welcome to Lilith's Garden
                     </p>
                     <Button id= 'watch-trailer' title='Watch Trailer' leftIcon={<TiLocationArrow/>} containerClass='bg-yellow-100 flex-center gap-1'/>
               </div>
