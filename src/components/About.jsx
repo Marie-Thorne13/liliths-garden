e2524903d9905eaa3774350082c1b7f5e16202dc
Toni Anguiano <toni.anguiano@sjsu.edu>
2025-01-14 17:39:14 -0800

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div id = 'about' className='min-h-screen w-screen'>About</div>
+    <div id = 'about' className='min-h-screen w-screen'>
+        <div className='relative mb-8 mt-36  flex flex-col 
+        items-center gap-5'>
+            <h2 className='font-general text-sm uppercase md:text[10px]'>
+
+            </h2>
+
+        </div>
+    </div>
   )
 }
 
